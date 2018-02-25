11d59a156ad9dd1393f72f5b0d935b19834bbc8b
John Hawthorn <john.hawthorn@gmail.com>
2018-02-25 14:27:34 -0800

    Add test for Availability

---
+++ test/availability_test.rb
@@ -0,0 +1,59 @@
+require 'test_helper'
+
+class AvailabilityTest < Minitest::Test
+  def setup
+    @spec = Galette::Specification.new('rack') do |s|
+      s.version '3'
+      s.version '2'
+      s.version '1'
+    end
+  end
+
+  def test_no_availability
+    availability = Galette::Availability.new(@spec, 0)
+
+    assert availability.none?
+    refute availability.one?
+    refute availability.multiple?
+
+    assert_raises do
+      availability.version
+    end
+  end
+
+  def test_single_availability
+    availability = Galette::Availability.new(@spec, 2)
+
+    refute availability.none?
+    assert availability.one?
+    refute availability.multiple?
+
+    assert_equal [@spec.versions[1]], availability.versions
+    assert_equal "3", availability.version.version
+  end
+
+  def test_multiple_availability
+    availability = Galette::Availability.new(@spec, 6)
+
+    refute availability.none?
+    refute availability.one?
+    assert availability.multiple?
+
+    assert_equal [@spec.versions[1], @spec.versions[2]], availability.versions
+
+    assert_raises do
+      availability.version
+    end
+  end
+
+  def test_unneeded
+    availability = Galette::Availability.new(@spec, 1)
+
+    refute availability.none?
+    assert availability.one?
+    refute availability.multiple?
+
+    assert_equal 1, availability.versions.length
+    assert availability.version.unneeded?
+  end
+end
